eae84f531ae39c9ab452dac17e0a7dc68ee45b11
Max <jmlong.dev@gmail.com>
2022-06-16 18:28:04 -0400

    extracted common css and createed a card component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,22 +1,16 @@
+import Card from "./Card";
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString("en-us", { month: "long" });
-  const year = props.date.getFullYear();
-  const day = props.date.toLocaleString("en-us", { day: "2-digit" });
-
   return (
-    <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
